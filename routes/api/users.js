c70cd525ebd14a1af1706e013deaa339621f641c
Kay Edwards <kaylaedwards575@gmail.com>
2022-08-30 13:53:40 -0400

    save

--- routes/api/users.js
+++ routes/api/users.js
@@ -86,37 +86,7 @@ router.route('/:id').delete((req,res) => {
     .catch(err => res.status(400).json('Error:' + err ))
 })
 
-// router.route('/add').post((req, res) => {
-//     User.findOne({ email: req.body.email})
-//     .then(user => {
-//         if (user) {
-//             return res.status(400).json({email:'email already exist'})
-//         }else{
-//             const avatar = gravatar.url(req.body.email, {
-//                 s: '200',
-//                 r:  'pg',
-//                 d:  'mm'
-//             })
-//             const newUser = new User({
-//                 name: req.body.name,
-//                 email: req.body.email,
-//                 avatar,
-//                 password: req.body.password
-//             });
 
-//             bcrypt.genSalt( 10, () => (err,salt) => {
-//                 bcrypt.hash(newUser.password, salt, (err, hash) => {
-//                     if(err) throw err;
-//                     newUser.password = hash;
-//                     newUser.save()
-//                        .then(user => res.json(user))
-//                        .catch(err => console.log(err))
-//                 })
-//             })
-//         }
-// })
-
-// });
 
 
 
